02c2c4b3bde95febc449c0866c06ae9c8a303e44
Dane Sherrets <Dane@Danes-MacBook-Air.local>
2017-11-28 11:44:48 -0500

    added button

--- server.js
+++ server.js
@@ -51,13 +51,16 @@ app.post('/twitterOutput', function(req, res) {
                     //   tweets[i]=$("#tweetholder");
                     // tweets[i].text("Results")
 
+                    res.write('<style> body{background:radial-gradient(circle farthest-side at 0% 50%,#fb1 23.5%,rgba(240,166,17,0) 0)21px 30px,radial-gradient(circle farthest-side at 0% 50%,#B71 24%,rgba(240,166,17,0) 0)19px 30px,linear-gradient(#fb1 14%,rgba(240,166,17,0) 0, rgba(240,166,17,0) 85%,#fb1 0)0 0,linear-gradient(150deg,#fb1 24%,#B71 0,#B71 26%,rgba(240,166,17,0) 0,rgba(240,166,17,0) 74%,#B71 0,#B71 76%,#fb1 0)0 0,linear-gradient(30deg,#fb1 24%,#B71 0,#B71 26%,rgba(240,166,17,0) 0,rgba(240,166,17,0) 74%,#B71 0,#B71 76%,#fb1 0)0 0,linear-gradient(90deg,#B71 2%,#fb1 0,#fb1 98%,#B71 0%)0 0 #fb1;background-size:40px 60px;} </style>');
                     res.write('<br>');
+                    res.write('<div style="background-color:4f5f66;" >');
                     res.write('<img src=' + p_image + '>');
                     res.write('<br>');
-                    res.write('<b>' + 'Name: ' + '</b>' + user_name + '<br>');
-                    res.write('<b>' + 'Screen Name: ' + '</b>' + '<a href="https://twitter.com/' + s_name + '">' + '@' + s_name + '</a>' + '<br>');
-                    res.write('<b>' + 'Message: ' + '</b>' + t_message + '<br>');
-                    res.write('<b>' + 'Location: ' + '</b>' + location + '<br>');
+                    res.write('<b style="color:ee8d1d">' + 'Name: ' + '</b>' + '<p style="color:white">'+ user_name + '</p>' + '<br>');
+                    res.write('<b style="color:ee8d1d">' + 'Screen Name: ' + '</b>' + '<a href="https://twitter.com/' + s_name + '">' + '@' + s_name + '</a>' + '<br>');
+                    res.write('<b style="color:ee8d1d">' + 'Message: ' + '</b>' + '<p style="color:white">'+ t_message +'</p>' + '<br>');
+                    res.write('</div>');
+                    // res.write('<b>' + 'Location: ' + '</b>' + location + '<br>');
                     res.write('<br>');
                 }
                 res.end();
